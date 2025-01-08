ce0003203cefac4cd39ffa2c7ad598a021c158c2
ftrav <ftraveller23@gmail.com>
2025-01-08 10:03:54 +0545

    failed deploymetn in github

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/Garden_of_words', // Your repo name
-    // Disable server actions for static export
-    experimental: {
-      serverActions: false,
-    }
-  }
-  
-  export default nextConfig
+const nextConfig = {};
+
+export default nextConfig;
