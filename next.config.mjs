102ec5da9a19e35ba56bce4b093e921e3fa3728a
ftrav <ftraveller23@gmail.com>
2025-01-08 10:11:14 +0545

    vercel req

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    experimental: {
+      serverActions: false, // Disable server actions
+    },
+    output: 'export', // Only if static export is required
+  };
+  
